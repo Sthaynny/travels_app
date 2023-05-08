2e97b1558ffd5fae634ad2b3de96e1f783ddbff3
Igor Sthanny <66264766+isthaynny@users.noreply.github.com>
2023-05-08 19:21:12 -0300

    feat:criando base projeto

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\software\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\sthay\Documents\Arquivos Flutter\travels_app"
+export "FLUTTER_ROOT=C:\Users\sthay\fvm\versions\3.7.12"
+export "FLUTTER_APPLICATION_PATH=C:\Users\sthay\Documents\GitHub\travels_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
